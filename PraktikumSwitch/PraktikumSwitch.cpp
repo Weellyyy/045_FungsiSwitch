fe603357a9356948b287c17d6cc9e08193797b2a
Weellyyy <wildanprtm2304@gmail.com>
2024-03-26 17:37:17 +0700

    menambahkan prosedur input

--- PraktikumSwitch/PraktikumSwitch.cpp
+++ PraktikumSwitch/PraktikumSwitch.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 using namespace std;
+
+int bilA, bil B;
+
+void input()
+{
+	cout << "Masukan bilangan Pertama = ";
+	cin >> bilA;
+	cout << "Masukan Bilangan Kedua = ";
+	cin << bil B;
+}
